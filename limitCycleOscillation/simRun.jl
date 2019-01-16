3459cb1705021349f7c2555f84a3998dd1592fc5
Kiran Ramesh <kiran.ramesh@glasgow.ac.uk>
2019-01-16 14:10:10 +0000

    add

---
+++ limitCycleOscillation/simRun.jl
@@ -0,0 +1,60 @@
+push!(LOAD_PATH,"../../src/")
+using UnsteadyFlowSolvers
+
+alpha_init = 10. *pi/180
+alphadot_init = 0.
+h_init = 0.
+hdot_init = 0.
+u = 0.467
+udot = 0
+kinem = KinemPar2DOF(alpha_init, h_init, alphadot_init, hdot_init, u)
+
+x_alpha = 0.05
+r_alpha = 0.5
+kappa = 0.05
+w_alpha = 1.
+w_h = 1.
+w_alphadot = 0.
+w_hdot = 0.
+cubic_h_1 = 1.
+cubic_h_3 = 0.
+cubic_alpha_1 = 1.
+cubic_alpha_3 = 0.
+strpar = TwoDOFPar(x_alpha, r_alpha, kappa, w_alpha, w_h, w_alphadot, w_hdot, cubic_h_1, cubic_h_3, cubic_alpha_1, cubic_alpha_3)
+
+#Kinematic definitions to initialise surface (initial conditions)
+alphadef = ConstDef(alpha_init)
+hdef = ConstDef(h_init)
+udef = ConstDef(1.) #This is relative to uref
+startkinem = KinemDef(alphadef, hdef, udef)
+
+lespcrit = [0.11;]
+
+pvt = 0.35
+
+c = 1.
+
+surf = TwoDSurf("FlatPlate", pvt, startkinem, lespcrit, c=c, uref=u)
+
+curfield = TwoDFlowField()
+
+dtstar = 0.015
+
+nsteps = 500
+
+t_tot = nsteps * dtstar / u
+
+startflag = 0
+
+writeflag = 0
+writeInterval = dtstar * nsteps/20.
+
+writeInterval = t_tot/20.
+
+delvort = delSpalart(500, 12, 1e-5)
+
+mat, surf, curfield = ldvm2DOF(surf, curfield, strpar, kinem, nsteps, dtstar,startflag, writeflag, writeInterval, delvort)
+
+makeForcePlots2D()
+
+#cleanWrite()
---
+++ surgeAirfoil/simRun.jl
@@ -0,0 +1,50 @@
+push!(LOAD_PATH,"../../src/")
+using UnsteadyFlowSolvers
+
+alphadef = ConstDef(13. *pi/180)
+
+hdef = ConstDef(0.)
+
+lambda = 0.5
+k = 0.1
+
+udef = SinDef(1., lambda, k, 0.)
+
+full_kinem = KinemDef(alphadef, hdef, udef)
+
+pvt = 0.25
+
+geometry = "FlatPlate"
+
+lespcrit = [0.25;]
+
+surf = TwoDSurf(geometry, pvt, full_kinem, lespcrit)
+
+curfield = TwoDFlowField()
+
+dtstar = 0.015 #UNSflow.find_tstep(hdef)
+
+t_tot = 2. *pi/udef.k
+
+nsteps = Int(round(t_tot/dtstar))+1
+
+println("nsteps", nsteps)
+
+startflag = 0
+
+writeflag = 1
+
+writeInterval = t_tot/10.
+
+#delvort = delSpalart(500, 12, 1e-5)
+delvort = UNSflow.delNone()
+
+mat, surf, curfield = ldvmLin(surf, curfield, nsteps, dtstar,startflag, writeflag, writeInterval, delvort)
+
+UNSflow.makeVortPlots2D()
+
+UNSflow.makeForcePlots2D()
+
+#UNSflow.makeKinemClVortPlots2D()
+
+UNSflow.cleanWrite()
